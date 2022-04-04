a7e76f00b830d0c49633d0e9c9356cc631bfcdc7
XavageHub <91792532+XavageHub@users.noreply.github.com>
2022-04-05 06:26:55 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ from discord_webhook import DiscordEmbed, DiscordWebhook
 
 import subprocess
 if os.name == "nt":
-    ctypes.windll.kernel32.SetConsoleTitleW(f"GEN AUNGPAO BY xenoz.xyz#8590")
+    ctypes.windll.kernel32.SetConsoleTitleW(f"GEN AUNGPAO BY xenoz.xyz#8590 X ") #หลัง Xใส่ชื่อตัวเองได้ ห้ามลบชื่อกู
 print("")
 def HWID():
     cmd = 'wmic csproduct get uuid'
@@ -34,7 +34,7 @@ def typewriter(message):
          time.sleep(0.1)
 typewriter(mess)
 
-hwid_auth = requests.get("https://pastebin.com/raw/f6JGg184")
+hwid_auth = requests.get("link hwid") #สร้างpastebin สำหรับเช๊คhwid/เพิ่ม-ลบhwid
 if HWID() in hwid_auth.text:
     time.sleep(5)
     os.system('cls')
@@ -48,10 +48,10 @@ if HWID() in hwid_auth.text:
             ░╚███╔╝░░░███╔═╝█████╗░░ 
             ░██╔██╗░██╔══╝░░██╔══╝░░ 
             ██╔╝╚██╗███████╗███████╗
-            ╚═╝░░╚═╝╚══════╝╚══════╝ """)
+            ╚═╝░░╚═╝╚══════╝╚══════╝ """) #แก้เป็นชื่อตัวเองได้
     
     
-    dis="Discord: https://discord.gg/mB4dkgGra9 \n"
+    dis="MADE BY: xenoz.xyz#8590 X  \n" #หลัง X ใส่ชื่อตัวเองได้ตรงนี้ห้ามลบชื่อกู
     def typewriter(message):
         for char in dis:
          sys.stdout.write(char)
@@ -66,10 +66,9 @@ if HWID() in hwid_auth.text:
         config = eval(contents)
         link = config['link']
         amount = int(input("amount: "))
-        front = input("3front: ")
-        behind = input("4behind: ")
+        
         for i in range(amount):
-            code = front + "".join(random.choices(string.digits, k=3)) + behind
+            code = https://gift.truemoney.com/campaign/?v= + "".join(random.choices(string.digits, k=18))
             print("[Generated] " + code)
             webhook = Webhook.from_url(link, adapter=RequestsWebhookAdapter())
             webhook.send(code)
@@ -80,7 +79,7 @@ if HWID() in hwid_auth.text:
 
 else:
     print("Your HWID was not found ")
-    print("Pls contact xenoz.xyz#8590 to add hwid")
+    print("Pls contact admin to add hwid")
     print("or join discord and open ticket")
     
     
